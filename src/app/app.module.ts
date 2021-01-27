a2317214c3294b98c580046ccb9521b81a9af4d5
Danilo Caneschi <caneschi.danilo@gmail.com>
2021-01-26 21:29:46 -0300

    Inclusão e visualização do todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {AdicionaItemPage} from '../pages/adiciona-item/adiciona-item';
+import {DetalheItemPage} from '../pages/detalhe-item/detalhe-item';
+import {DataProvider} from '../providers/data/data';
+import {IonicStorageModule} from "@ionic/storage";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AdicionaItemPage,
+    DetalheItemPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AdicionaItemPage,
+    DetalheItemPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataProvider
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ModalController, NavController} from 'ionic-angular';
+import {DataProvider} from '../../providers/data/data';
+import {AdicionaItemPage} from '../adiciona-item/adiciona-item';
+import {DetalheItemPage} from "../detalhe-item/detalhe-item";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public items;
 
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public dataService: DataProvider) {
+    this.dataService.getData().then((todos) => {
+      if (todos) {
+        this.items = todos;
+      }
+    });
   }
 
+  ionViewDidLoad() {
+    this.items = [
+      {titulo: "todo 1", descricao: "tarefa 1"},
+      {titulo: "todo 2", descricao: "tarefa 2"},
+      {titulo: "todo 3", descricao: "tarefa 3"}
+    ];
+  }
+
+  adicionarItem() {
+    let addModal = this.modalCtrl.create(AdicionaItemPage);
+
+    addModal.onDidDismiss((item) => {
+      if (item) {
+        this.items.push(item);
+        this.dataService.salvarData(this.items);
+      }
+    });
+
+    addModal.present();
+  }
+
+  exibirItem(item) {
+    this.navCtrl.push(DetalheItemPage, {
+      item: item
+    });
+  }
+
+  excluirItem(item) {
+    let indice = this.items.findIndex(item);
+    this.items.slice(indice, 1);
+    this.dataService.salvarData(this.items);
+  }
 }
